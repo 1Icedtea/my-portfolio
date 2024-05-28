735e8c4a29bfaebc4a6752b2d09a6bb919f5477b
IcedTea <fistfireace37@gmail.com>
2024-05-28 19:42:06 +0800

    added floating nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { FloatingNav } from "@/components/FloatingNav";
 import Hero from "@/components/Hero";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main>
       <div className="w-full">
+        <FloatingNav navItems={navItems} />
         <Hero />
       </div>
     </main>
--- data/index.ts
+++ data/index.ts
@@ -1,3 +1,9 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
 export const socialMedia = [
   {
     id: 1,
